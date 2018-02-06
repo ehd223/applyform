af5071ff16021d7725bbb2e5a99c4863a59a56d0
ehd223 <ehd223@naver.com>
2018-02-06 19:52:11 +0900

    smsm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -89,5 +88,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 end
