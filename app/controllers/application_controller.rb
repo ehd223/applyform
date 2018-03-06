7be1b63a7d2cee5ba4666e1358a62d14c44e0827
ehd223 <ehd223@naver.com>
2018-03-06 12:45:11 +0900

    auth bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def authorize
-    redirect_to 'login' unless current_user
+    redirect_to '/apply/login' unless current_user
   end
 
 end
